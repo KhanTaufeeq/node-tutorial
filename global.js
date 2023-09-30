868e4cdc38759c1c3233a24b885d6c90a1c92436
khanTaufeeq <taufeeqyouth@gmail.com>
2023-09-30 22:11:55 +0530

    global objects

---
+++ global.js
@@ -0,0 +1,18 @@
+// console.log('node');
+// global object
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log('setTimeout');
+//     clearInterval(int);
+// }, 5000);
+
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000);
+
+console.log(__dirname);  // return absolute path of current folder
+console.log(__filename); // return absolute path of current file
+
+console.log(document);
+// document is window object. It is not defined in global namespace of node environment
